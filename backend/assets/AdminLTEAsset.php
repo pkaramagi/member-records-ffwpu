d55d2219ce5d3b30c2e9849350504ed940309067
Phillip Karamagi <pkaramagi1@gmail.com>
2016-07-21 16:12:37 +0300

    Added AdminLTE template and AdminLTEAsset

---
+++ backend/assets/AdminLTEAsset.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Philip
+ * Date: 7/21/2016
+ * Time: 4:07 PM
+ */
+
+namespace backend\assets;
+use yii\web\AssetBundle;
+
+
+class AdminLTEAsset extends  AssetBundle
+{
+
+}
