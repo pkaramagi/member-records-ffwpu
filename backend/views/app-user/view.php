0c264ade95383bcf3d629de3caca262e3b665ef8
Phillip Karamagi <pkaramagi1@gmail.com>
2016-07-22 13:09:07 +0300

    Added logo to layout/main.php in theme

--- backend/views/app-user/view.php
+++ backend/views/app-user/view.php
@@ -5,46 +5,65 @@ use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\AppUser */
-
-$this->title = $model->id;
+/*change title to firstname and last name*/
+$this->title = $model->last_name.' '.$model->first_name;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'App Users'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="app-user-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <!--<h1><?= Html::encode($this->title) ?></h1> -->
 
     <p>
         <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
         <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+                'confirm' => Yii::t('app', 'Are you sure you want to delete this User?'),
                 'method' => 'post',
             ],
         ]) ?>
     </p>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'first_name',
-            'middle_name',
-            'last_name',
-            'username',
-            'nationality',
-            'blessing_group_id',
-            'date_of_birth',
-            'spiritual_date_of_birth',
-            'spiritual_parent',
-            'passport',
-            'picture',
-            'joined_at',
-            'sex',
-            'religion_id',
-            'generation_id',
-        ],
-    ]) ?>
+    <div class="app-user-index box box-primary">
+        <!-- /.box-header -->
+        <div class="box-header with-border">
+            <h3 class="box-title">Details of <?= Html::encode($this->title) ?></h3>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+            <?= DetailView::widget([
+                'model' => $model,
+                'attributes' => [
+                    'id',
+                    'first_name',
+                    'middle_name',
+                    'last_name',
+                    'username',
+                    'nationality',
+                    [                      // the blessing group of the model
+                        'label' => 'Blessing Group',
+                        'value' => $model->blessingGroup->name,
+                    ],
+                    'date_of_birth',
+                    'spiritual_date_of_birth',
+                    'spiritual_parent',
+                    'passport',
+                    'picture',
+                    'joined_at',
+                    'sex',
+                   // 'religion_id',
+                    [                      // the former religion of the model
+                        'label' => 'Former Religion',
+                        'value' => $model->religion->name,
+                    ],
+                   // 'generation_id',
+                    [                      // the generation of the model
+                        'label' => 'Genenation',
+                        'value' => $model->generation->name,
+                    ],
+                ],
+            ]) ?>
 
-</div>
+        </div>
+    </div>
