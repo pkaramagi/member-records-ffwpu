37de372c5ef5321adbee1ef4a22918302df00812
Phillip Karamagi <pkaramagi1@gmail.com>
2016-07-22 09:40:45 +0300

    Worked on app-user crud forms

--- backend/views/app-user/_form.php
+++ backend/views/app-user/_form.php
@@ -13,68 +13,68 @@ use kartik\widgets\FileInput;
 <div class="app-user-form box box-primary">
 
     <?php $form = ActiveForm::begin(); ?>
-<div class="box-body">
-    <?= $form->field($model, 'first_name')->textInput(['maxlength' => true]) ?>
+    <div class="box-body">
+        <?= $form->field($model, 'first_name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'middle_name')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'middle_name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'last_name')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'last_name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
 
-    <?=$form->field($model, 'nationality')->widget(Select2::classname(), [
-        'data' => $model->getNationalities(),
-        'options' => ['placeholder' => 'Nationality ...'],
-    ])->label('Nationality'); ?>
+        <?=$form->field($model, 'nationality')->widget(Select2::classname(), [
+            'data' => $model->getNationalities(),
+            'options' => ['placeholder' => 'Nationality ...'],
+        ])->label('Nationality'); ?>
 
-    <?=$form->field($model, 'blessing_group_id')->widget(Select2::classname(), [
-        'data' => $blessing_groups,
-        'options' => ['placeholder' => 'Select a blessing group ...'],
-        'pluginOptions' => [
-            'tags' => true,
-            'maximumInputLength' => 2
-        ],
+        <?=$form->field($model, 'blessing_group_id')->widget(Select2::classname(), [
+            'data' => $blessing_groups,
+            'options' => ['placeholder' => 'Select a blessing group ...'],
+            'pluginOptions' => [
+                'tags' => true,
+                'maximumInputLength' => 2
+            ],
 
-    ])->label('Blessing Group'); ?>
+        ])->label('Blessing Group'); ?>
 
-    <?= $form->field($model, 'date_of_birth')->widget(DatePicker::classname(), [
-    'options' => ['placeholder' => 'Enter birth date ...'],
-    'pluginOptions' => [
-    'autoclose'=>true
-    ]
-    ]); ?>
+        <?= $form->field($model, 'date_of_birth')->widget(DatePicker::classname(), [
+            'options' => ['placeholder' => 'Enter birth date ...'],
+            'pluginOptions' => [
+                'autoclose'=>true
+            ]
+        ]); ?>
 
-    <?= $form->field($model, 'spiritual_date_of_birth')->widget(DatePicker::classname(), [
-        'options' => ['placeholder' => 'Enter birth date ...'],
-        'pluginOptions' => [
-            'autoclose'=>true
-        ]
-    ])->label('Spiritual Birth Date'); ?>
-
-
-    <?= $form->field($model, 'spiritual_parent')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'passport')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'picture')->widget(FileInput::classname(), [
-        'options' => ['accept' => 'image/*'],
-    ]); ?>
-
-    <?= $form->field($model, 'joined_at')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'sex')->radioList(array('M'=>'Male','F'=>'Female'))->label('Gender'); ?>
-
-    <?=$form->field($model, 'religion_id')->widget(Select2::classname(), [
-        'data' => $religions,
-        'options' => ['placeholder' => 'Previous Religion ...'],
-    ])->label('Previous Religion'); ?>
+        <?= $form->field($model, 'spiritual_date_of_birth')->widget(DatePicker::classname(), [
+            'options' => ['placeholder' => 'Enter birth date ...'],
+            'pluginOptions' => [
+                'autoclose'=>true
+            ]
+        ])->label('Spiritual Birth Date'); ?>
 
 
-    <?=$form->field($model, 'generation_id')->widget(Select2::classname(), [
-        'data' => $generations,
-        'options' => ['placeholder' => 'Generation ...'],
-    ])->label('Generation'); ?>
-</div>
+        <?= $form->field($model, 'spiritual_parent')->textInput(['maxlength' => true]) ?>
+
+        <?= $form->field($model, 'passport')->textInput(['maxlength' => true]) ?>
+
+        <?= $form->field($model, 'picture')->widget(FileInput::classname(), [
+            'options' => ['accept' => 'image/*'],
+        ]); ?>
+
+        <?= $form->field($model, 'joined_at')->textInput(['maxlength' => true]) ?>
+
+        <?= $form->field($model, 'sex')->radioList(array('M'=>'Male','F'=>'Female'))->label('Gender'); ?>
+
+        <?=$form->field($model, 'religion_id')->widget(Select2::classname(), [
+            'data' => $religions,
+            'options' => ['placeholder' => 'Previous Religion ...'],
+        ])->label('Previous Religion'); ?>
+
+
+        <?=$form->field($model, 'generation_id')->widget(Select2::classname(), [
+            'data' => $generations,
+            'options' => ['placeholder' => 'Generation ...'],
+        ])->label('Generation'); ?>
+    </div>
     <div class="form-group box-footer">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
--- backend/views/app-user/index.php
+++ backend/views/app-user/index.php
@@ -10,38 +10,45 @@ use yii\widgets\Pjax;
 $this->title = Yii::t('app', 'App Users');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="app-user-index">
 
 <!--    <h1><?= Html::encode($this->title) ?></h1> -->
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+<?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+<p>
+    <?= Html::a(Yii::t('app', 'Create App User'), ['create'], ['class' => 'btn btn-success']) ?>
+</p>
+<div class="app-user-index box box-primary">
+    <!-- /.box-header -->
+    <div class="box-header with-border">
+        <h3 class="box-title">List of Members</h3>
+    </div>
+    <!-- /.box-header -->
+    <div class="box-body">
+        <?php Pjax::begin(); ?>    <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
 
-    <p>
-        <?= Html::a(Yii::t('app', 'Create App User'), ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-<?php Pjax::begin(); ?>    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+                'id',
+                'first_name',
+                'middle_name',
+                'last_name',
+                'username',
+                // 'nationality',
+                // 'blessing_group_id',
+                // 'date_of_birth',
+                // 'spiritual_date_of_birth',
+                // 'spiritual_parent',
+                // 'passport',
+                // 'picture',
+                // 'joined_at',
+                // 'sex',
+                // 'religion_id',
+                // 'generation_id',
 
-            'id',
-            'first_name',
-            'middle_name',
-            'last_name',
-            'username',
-            // 'nationality',
-            // 'blessing_group_id',
-            // 'date_of_birth',
-            // 'spiritual_date_of_birth',
-            // 'spiritual_parent',
-            // 'passport',
-            // 'picture',
-            // 'joined_at',
-            // 'sex',
-            // 'religion_id',
-            // 'generation_id',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-<?php Pjax::end(); ?></div>
+                ['class' => 'yii\grid\ActionColumn'],
+            ],
+        ]); ?>
+        <?php Pjax::end(); ?>
+    </div><!-- /.box-body -->
+</div>
